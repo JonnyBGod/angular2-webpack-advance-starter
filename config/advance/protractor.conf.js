f13a70fb4aae061f2cd1f625a81b7739a6c8053e
Joao Ribeiro <jonnybgod@gmail.com>
2016-06-01 06:26:24 +0100

    fix e2e tests

---
+++ config/advance/protractor.conf.js
@@ -0,0 +1,55 @@
+/**
+ * @author: @AngularClass
+ */
+
+//require('ts-node/register');
+var helpers = require('./helpers');
+
+exports.config = {
+  baseUrl: 'http://localhost:3000/',
+
+  // use `npm run e2e`
+  specs: [
+    helpers.root('_temp/tests/**/**.e2e.js'),
+    helpers.root('_temp/tests/**/*.e2e.js')
+  ],
+  exclude: [],
+
+  framework: 'jasmine2',
+
+  allScriptsTimeout: 110000,
+
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+  },
+  directConnect: true,
+
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['show-fps-counter=true']
+    }
+  },
+
+  onPrepare: function() {
+    require("reflect-metadata");
+    require("zone.js/dist/zone-node");
+
+    const SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: true }));
+    browser.ignoreSynchronization = true;
+  },
+
+  /**
+   * Angular 2 configuration
+   *
+   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+   * `rootEl`
+   */
+   useAllAngular2AppRoots: true
+};
